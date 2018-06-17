b41e0e9a264641e9a5f3bd19c55298cecd6542af
Rafael Marighetti <rafael.marighetti@gmail.com>
2018-06-16 23:09:10 -0300

    Conteúdo de IONIC em teste

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { HomePage } from '../home/home';
+import { FeedPage } from '../feed/feed';
+import { IntroPage } from '../intro/intro';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab4Root = FeedPage;
+  tabs5Root = IntroPage;
+
+  constructor() {
+
+  }
+}
